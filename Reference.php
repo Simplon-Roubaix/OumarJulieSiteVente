6e22d99628331c770523e39422abedcdb06f9851
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-07 14:47:40 +0200

    Add Change to End Of Array

--- Reference.php
+++ Reference.php
@@ -1,15 +1,13 @@
 <?php
 
 $Reference = [
-  ['Titre' => 'Telephone1', 'Description' => 'BlaBla1', 'Image' => '', 'Texte' => 'Texte1'];
+  ['Titre' => 'Telephone1', 'Description' => 'BlaBla1', 'Image' => '', 'Texte' => 'Texte1'],
 
-  ['Titre' => 'Telephone2', 'Description' => 'BlaBla2', 'Image' => '', 'Texte' => 'Texte2'];
+  ['Titre' => 'Telephone2', 'Description' => 'BlaBla2', 'Image' => '', 'Texte' => 'Texte2'],
 
-  ['Titre' => 'Telephone3', 'Description' => 'BlaBla3', 'Image' => '', 'Texte' => 'Texte3'];
+  ['Titre' => 'Telephone3', 'Description' => 'BlaBla3', 'Image' => '', 'Texte' => 'Texte3'],
 
 ];
-
-
 
 
 

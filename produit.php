1243d4431483a4f2de49529f4226c688f2447e21
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-08 10:28:56 +0200

    Add Main to Produit page

--- produit.php
+++ produit.php
@@ -6,8 +6,24 @@
   </head>
   <body>
 
-<?php include('header.php'): ?>
-<?php include('footer.php'): ?>
+
+    <?php
+    include "Reference.php";
+    	 ?>
+
+
+<?php //include("header.php"); ?>
+<?php //include("footer.php"); ?>
+
+<article class="card" style="width: 20rem;">
+  <figure class="imageBonbon">
+    <img class="card-img-top" src="<?php echo $Reference[1]['Image']; ?>" alt="Card image cap">
+  </figure>
+    <!-- <div class="card-block">
+      <h4 class="card-title"><?php echo $value['Titre']?></h4>
+      <p class="card-text"><?php echo $value['Description']?></p>
+    </div> -->
+</article>
 
 
   </body>

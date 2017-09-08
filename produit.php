c945b40de43297de7d63280ad06680395046cd26
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-08 14:15:35 +0200

    Add Fisrt page

--- produit.php
+++ produit.php
@@ -18,17 +18,19 @@
 <div id="produit">
 
 
-  <figure class="imageBonbon">
-    <img class="card-img-top" src="<?php echo $Reference[1]['Image']; ?>" alt="Card image cap">
-  </figure>
-  <h2><?php echo $Reference[1]['Titre']; ?></h2>
+     <figure class="imageBonbon">
+        <img class="card-img-top" src="<?php echo $Reference[1]['Image']; ?>" alt="Card image cap">
+    </figure>
 
-    <!-- <div class="card-block">
-      <h4 class="card-title"><?php echo $value['Titre']?></h4>
-      <p class="card-text"><?php echo $value['Description']?></p>
-    </div> -->
+   <div id="ContainerProduit">
 
-    <p><?php echo $Reference[1]['Description'];?></p>
+        <h2><?php echo $Reference[1]['Titre']; ?></h2><br/ >
+
+
+
+       <p><?php echo $Reference[1]['Description'];?></p>
+   </div>
+
   </div>
 
 

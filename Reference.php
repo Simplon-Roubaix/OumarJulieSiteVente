e984ff7fed6ff9bcbffe83576b86389ffbc9348a
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-11 15:06:51 +0200

    Add link

--- Reference.php
+++ Reference.php
@@ -1,19 +1,19 @@
 <?php
 
-$Reference = [
-  ['Titre' => 'Bonbons nounours', 'Description' => 'De la marque Haribo, ces bonbons plairont aux petits comme aux grands', 'Image' => 'img/bonbon1.jpg', 'Texte' => 'Tendres et savoureux, dégustez les bonbons ours que les enfants d\'aujourd\'hui prennent toujours autant de plaisir à dévorer.
+$Reference = array(
+  0=> ['Titre' => 'Bonbons nounours', 'Description' => 'De la marque Haribo, ces bonbons plairont aux petits comme aux grands', 'Image' => 'img/bonbon1.jpg', 'Texte' => 'Tendres et savoureux, dégustez les bonbons ours que les enfants d\'aujourd\'hui prennent toujours autant de plaisir à dévorer.
 	  Craquez pour les célèbres bonbons Ours HARIBO !
    	Ce lot contient 3 sachets de HARIBO L\'Ours d\'Or 120g.
 	Le saviez-vous ? Le premier bonbon qu\'inventa le créateur de HARIBO en 1920 fut l\'Ours d\'Or.'],
 
-  ['Titre' => 'Dragibus', 'Description' => 'Ces petites boules de sucre colorées vous feront retomber en enfance', 'Image' => 'img/bonbon2.jpg', 'Texte' => 'Depuis 1973, Haribo vous propose un grand nombre de parfums de Dragibus. Plusieurs générations ont grandi avec ces bonbons qui représentent un grand classique. Si les années ont passé, l\'intérêt que portent les plus ou moins jeunes à ce bonbon Haribo n\'a en rien changé. Ce dernier se décline en une multitude de couleurs pour le plus grand plaisir des gourmands avides de nouveautés.'],
+  1=> ['Titre' => 'Dragibus', 'Description' => 'Ces petites boules de sucre colorées vous feront retomber en enfance', 'Image' => 'img/bonbon2.jpg', 'Texte' => 'Depuis 1973, Haribo vous propose un grand nombre de parfums de Dragibus. Plusieurs générations ont grandi avec ces bonbons qui représentent un grand classique. Si les années ont passé, l\'intérêt que portent les plus ou moins jeunes à ce bonbon Haribo n\'a en rien changé. Ce dernier se décline en une multitude de couleurs pour le plus grand plaisir des gourmands avides de nouveautés.'],
 
-  ['Titre' => 'Sucettes', 'Description' => 'Ces sucettes sont les plus colorées au monde', 'Image' => 'img/bonbon3.jpg', 'Texte' => 'Un large choix de sucettes de la marque Chupa Chups, mais aussi une sélection de sucettes fantaisistes et d\'autres marques comme les sucettes pierrot gourmand. L\'invention des sucettes modernes est encore un mystère. Les premières confiseries qui ressemblent beaucoup à ce que nous appelons les sucettes datent du Moyen Âge, lorsque des gens de haut rang notamment la noblesse mangent souvent du sucre bouilli à l\'aide de bâtons ou de poignées.'],
+  2=> ['Titre' => 'Sucettes', 'Description' => 'Ces sucettes sont les plus colorées au monde', 'Image' => 'img/bonbon3.jpg', 'Texte' => 'Un large choix de sucettes de la marque Chupa Chups, mais aussi une sélection de sucettes fantaisistes et d\'autres marques comme les sucettes pierrot gourmand. L\'invention des sucettes modernes est encore un mystère. Les premières confiseries qui ressemblent beaucoup à ce que nous appelons les sucettes datent du Moyen Âge, lorsque des gens de haut rang notamment la noblesse mangent souvent du sucre bouilli à l\'aide de bâtons ou de poignées.'],
 
-  ['Titre' => 'Berlingots', 'Description' => 'Retrouvez les bonbons traditionnels d\'antan', 'Image' => 'img/bonbon4.jpg', 'Texte' => 'Clément V s\'appelant Bertrand de Goth, le bonbon reçu le nom de Berlingot...
+  3=> ['Titre' => 'Berlingots', 'Description' => 'Retrouvez les bonbons traditionnels d\'antan', 'Image' => 'img/bonbon4.jpg', 'Texte' => 'Clément V s\'appelant Bertrand de Goth, le bonbon reçu le nom de Berlingot...
 
 	C\'est en 1844 que le 1er berlingot "moderne" fut créé à Carpentras par le pâtissier François-Pascal LONG. Ce pâtissier aurait eu l\'idée d\'utiliser le sirop d\'egouttage des fruits confits, qu\'il fit cuire, le parfuma à la menthe et eut l\'idée de décorer la pâte avec un sucre blanchi. Il découpa ensuite la pâte encore chaude avec des ciseaux pour lui donner la forme que nous connaissons aujourd\'hui.']
-];
+);
 
 
 

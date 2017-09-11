b0879159edfbd30789590a81adb02cd414a2e361
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-11 14:13:38 +0200

    Add footerinfo

--- footerInfo.php
+++ footerInfo.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 $footer_info = [
-'titre_footer' => 'Retrouvez nous sur les réseaux !', 
+'titre_footer' => 'Retrouvez nous sur les réseaux !',
 'icon_footer' => '<i class="fa fa-facebook-square" aria-hidden="true"></i>
             <i class="fa fa-twitter-square" aria-hidden="true"></i>
             <i class="fa fa-pinterest-square" aria-hidden="true"></i>'

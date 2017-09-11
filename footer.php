c699461e86aacdfe3528809ab1149fed73512b84
Julie.Canevet <julie.canevet@hotmail.fr>
2017-09-11 14:29:25 +0200

    add link footer

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
-    <?php include 'footerinfo.php' ?>
+    <?php include 'footerInfo.php' ?>
     <link rel = "stylesheet" href = "font-awesome-4.7.0/css/font-awesome.min.css">
 
-
-
-
-  <body>
    <footer id="footer">
       <p><?php
         echo $footer_info['titre_footer'];

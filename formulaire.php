6e25b71a04d5af874a09ffe3b65afa472e195054
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-18 16:17:29 +0200

    Add button and label

--- formulaire.php
+++ formulaire.php
@@ -5,6 +5,11 @@
     <title>Ajouter un nouvel article</title>
   </head>
   <body>
-
+<form class="" action="" method="post">
+<label for="titre">Titre</label></label> <input type="text" name="Titre" value=""><br />
+<label for="titre">Accroche</label></label> <input type="text" name="Accroche" value=""><br />
+<label for="titre">Description</label></label> <input type="text" name="Description" value=""><br />
+<input type="file" name="fichier"/>
+</form>
   </body>
 </html>

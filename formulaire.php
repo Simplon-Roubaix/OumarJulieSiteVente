cebd02d2fd5d10bd497af7b23be059b08745bc48
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-18 16:27:17 +0200

    Add button submit

--- formulaire.php
+++ formulaire.php
@@ -5,11 +5,29 @@
     <title>Ajouter un nouvel article</title>
   </head>
   <body>
+
+
 <form class="" action="" method="post">
 <label for="titre">Titre</label></label> <input type="text" name="Titre" value=""><br />
 <label for="titre">Accroche</label></label> <input type="text" name="Accroche" value=""><br />
 <label for="titre">Description</label></label> <input type="text" name="Description" value=""><br />
-<input type="file" name="fichier"/>
+<input type="file" name="fichier"/><br />
+<button type="submit" name="button">Créer</button>
+
 </form>
+
+<?php
+
+try {
+
+$bdd = new PDO('mysql:host=localhost;dbname=siteBonbon;charset=utf8', 'root', 'root');
+
+} catch (Exception $e) {
+die('Erreur : ' . $e->getMessage());
+}
+
+
+ ?>
+
   </body>
 </html>
